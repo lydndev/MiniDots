85660be0f143d731a5fc669e48ca5bcbbe5e05de
lydndev <sokilkozakov12@gmail.com>
2026-07-22 16:35:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -29,13 +29,29 @@ const UI = {
 
     screens: {
         mainMenu: document.getElementById("mainMenu"),
-        modeMenu: document.getElementById("modeMenu")
+        modeMenu: document.getElementById("modeMenu"),
+        roomMenu: document.getElementById("roomMenu")
     },
 
     buttons: {
-        play: document.getElementById("playButton"),
-        back: document.getElementById("backToMainMenuButton")
-    }
+
+    play: document.getElementById("playButton"),
+
+    back: document.getElementById("backToMainMenuButton"),
+
+    mode1v1: document.getElementById("mode1v1Button"),
+
+    mode3v3: document.getElementById("mode3v3Button"),
+
+    mode5v5: document.getElementById("mode5v5Button"),
+
+    createRoom: document.getElementById("createRoomButton"),
+
+    joinRoom: document.getElementById("joinRoomButton"),
+
+    backToMode: document.getElementById("backToModeMenuButton")
+
+}
 
 };
 
@@ -98,3 +114,35 @@ function checkOrientation() {
 window.addEventListener("resize", checkOrientation);
 
 checkOrientation();
+
+//==================================================
+// ВЫБОР РЕЖИМА
+//==================================================
+
+UI.buttons.mode1v1.addEventListener("click", () => {
+
+    showScreen("roomMenu");
+
+});
+
+UI.buttons.mode3v3.addEventListener("click", () => {
+
+    showScreen("roomMenu");
+
+});
+
+UI.buttons.mode5v5.addEventListener("click", () => {
+
+    showScreen("roomMenu");
+
+});
+
+//==================================================
+// НАЗАД
+//==================================================
+
+UI.buttons.backToMode.addEventListener("click", () => {
+
+    showScreen("modeMenu");
+
+});
