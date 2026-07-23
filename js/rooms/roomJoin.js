a65544c731aaf6ac46db607acf34731017c6f254
lydndev <sokilkozakov12@gmail.com>
2026-07-23 19:54:51 +0300

    Update roomJoin.js

--- js/rooms/roomJoin.js
+++ js/rooms/roomJoin.js
@@ -18,3 +18,51 @@ function openJoinRoom() {
     showScreen("joinRoomScreen");
 
 }
+
+//==================================================
+// ПОИСК КОМНАТЫ
+//==================================================
+
+async function findRoom(code) {
+
+    const response = await fetch(
+        `${FIREBASE_URL}/rooms/${code}.json`
+    );
+
+    return await response.json();
+
+}
+
+//==================================================
+// ВХОД В КОМНАТУ
+//==================================================
+
+async function joinRoom() {
+
+    const code = document
+        .getElementById("joinRoomInput")
+        .value
+        .trim()
+        .toUpperCase();
+
+    if (code.length !== 6) {
+
+        alert("Введите код комнаты");
+
+        return;
+
+    }
+
+    const room = await findRoom(code);
+
+    if (!room) {
+
+        alert("Комната не найдена");
+
+        return;
+
+    }
+
+    alert("Комната найдена!");
+
+}
