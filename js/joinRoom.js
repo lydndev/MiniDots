204c53e278418964459de3c0f5cd731855341d3a
lydndev <sokilkozakov12@gmail.com>
2026-07-23 18:59:28 +0300

    Update joinRoom.js

--- js/joinRoom.js
+++ js/joinRoom.js
@@ -1,1 +1,38 @@
-.
+import { db } from "./firebase.js";
+
+import {
+    ref,
+    get
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
+
+async function joinRoom() {
+
+    const code = document
+        .getElementById("joinRoomInput")
+        .value
+        .trim()
+        .toUpperCase();
+
+    if (code === "") {
+
+        alert("Введите код комнаты");
+
+        return;
+
+    }
+
+    const roomRef = ref(db, "rooms/" + code);
+
+    const snapshot = await get(roomRef);
+
+    if (!snapshot.exists()) {
+
+        alert("Комната не найдена");
+
+        return;
+
+    }
+
+    alert("Комната найдена!");
+
+}
