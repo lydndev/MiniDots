eddbd24a745a46fe4fc2145c4a6b0a9339a620ce
lydndev <sokilkozakov12@gmail.com>
2026-07-23 14:07:58 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,11 @@
+// Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCf_Kx_ASA5F-ym8KKi3KwSh8Yo1FPuiUg",
     authDomain: "minidots-ee1de.firebaseapp.com",
+    databaseURL: "https://minidots-ee1de-default-rtdb.firebaseio.com",
     projectId: "minidots-ee1de",
     storageBucket: "minidots-ee1de.firebasestorage.app",
     messagingSenderId: "717424414180",
@@ -12,6 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const database = getDatabase(app);
-
-window.database = database;
+export const db = getDatabase(app);
