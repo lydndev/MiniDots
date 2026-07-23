771bde49d61fe068cc1bca7d6c651467b4bed637
lydndev <sokilkozakov12@gmail.com>
2026-07-23 14:12:02 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-m
+import "./firebase.js";
+
+import "./main.js";
+import "./screens.js";
+import "./modal.js";
+import "./fullscreen.js";
+import "./orientation.js";
+import "./room.js";
+import "./navigation.js";
+import "./gameMode.js";
+import "./buttons.js";
