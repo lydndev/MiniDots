31d341b8893d3b75af0c75ce4c341b85257239a9
lydndev <sokilkozakov12@gmail.com>
2026-07-23 13:16:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,33 +80,6 @@ const Game = {
 
 };
 
-//==================================================
-// ПРОВЕРКА ОРИЕНТАЦИИ
-//==================================================
-
-function checkOrientation() {
-
-    if (window.innerHeight > window.innerWidth) {
-
-        UI.rotateScreen.style.display = "flex";
-
-        for (const screen of Object.values(UI.screens)) {
-
-            screen.style.display = "none";
-
-        }
-
-    } else {
-
-        UI.rotateScreen.style.display = "none";
-
-        showScreen(Game.currentScreen);
-
-    }
-
-}
-
-window.addEventListener("resize", checkOrientation);
 
 //==================================================
 // ОБРАБОТКА КНОПОК
